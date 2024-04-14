31a667dbd10f4cb0a44d489ae853bbe501e8b5da
cvijander <cvija85@gmail.com>
2024-04-14 13:57:27 +0200

    Variables

---
+++ variables.js
@@ -0,0 +1,15 @@
+let x;
+ // console.log(x);
+x =7;
+x =6;
+x=5;
+x =4;
+let y =3;
+let z =x +y;
+console.log('Answer ' + z);
+
+
+let firstNumber =14;
+let secondNumber =21;
+let zipCode ='11211';
+let firstName ='Bob';
